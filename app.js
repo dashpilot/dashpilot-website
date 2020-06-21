43633c53ca2b735dbfebcc4c563d06ceaa11f534
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-22 00:46:35 +0200

    started creating the docs

---
+++ app.js
@@ -0,0 +1,19 @@
+var post = document.querySelector("#post").innerHTML;
+var template = Handlebars.compile(post);
+
+fetch("api/data.json").then((response) => response.json()).then(function(data) {
+    console.log(data);
+    var converter = new showdown.Converter();
+
+    // preprocess markdown
+    var i = 0;
+    data.entries.forEach(function(item) {
+        data.entries[i].body = converter.makeHtml(item.body);
+
+        i++;
+    });
+
+    var html = template(data);
+
+    document.querySelector("#main").innerHTML = html;
+});
