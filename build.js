5336fba45296fa705f346692fb8a2f6abb02c0e4
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-23 22:35:57 +0200

    fix

--- build.js
+++ build.js
@@ -15,8 +15,8 @@ data.entries.forEach(function(item) {
 });
 
 // docs
-let mydata = {}
-let mydata.entries = data.entries.filter(x => x.category == 'docs');
+let mydata = {};
+mydata.entries = data.entries.filter((x) => x.category == "docs");
 
 var template = fs.readFileSync("./src/docs.html", "utf8");
 var pageBuilder = handlebars.compile(template);
