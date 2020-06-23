5d3d2bd0096ec7b93190c29aad543ac8f8706972
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-23 20:07:43 +0200

    preprocess markdown

--- build.js
+++ build.js
@@ -1,9 +1,20 @@
 var fs = require("fs");
 var handlebars = require("handlebars");
+var showdown = require("showdown");
+const converter = new showdown.Converter();
 
+var category = "docs";
 var template = fs.readFileSync("./src/docs.html", "utf8");
 var data = fs.readFileSync("./content/data.json", "utf8");
 data = JSON.parse(data);
+
+// preprocess markdown
+var i = 0;
+data.entries.forEach(function(item) {
+    data.entries[i].body = converter.makeHtml(item.body);
+    console.log("ok");
+    i++;
+});
 
 var pageBuilder = handlebars.compile(template);
 var pageText = pageBuilder(data);
