72696bd6b1c3e6a677e916852cf3d4773770698c
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-24 00:37:22 +0200

    improving the build script

--- build.js
+++ build.js
@@ -17,11 +17,19 @@ data.entries.forEach(function(item) {
 // docs
 let mydata = {};
 mydata.entries = data.entries.filter((x) => x.category == "docs");
+compile(mydata, "template.html", "docs.html");
 
-var template = fs.readFileSync("./src/docs.html", "utf8");
-var pageBuilder = handlebars.compile(template);
-var pageText = pageBuilder(mydata);
-fs.writeFileSync("./public/docs.html", pageText, "utf8");
+// blog
+mydata.entries = data.entries.filter((x) => x.category == "blog");
+compile(mydata, "template.html", "blog.html");
 
+// copy style
 var style = fs.readFileSync("./src/assets/docs.css", "utf8");
 fs.writeFileSync("./public/assets/docs.css", style, "utf8");
+
+function compile(mydata, src, dest) {
+    var template = fs.readFileSync("./src/" + src, "utf8");
+    var pageBuilder = handlebars.compile(template);
+    var pageText = pageBuilder(mydata);
+    fs.writeFileSync("./public/" + dest, pageText, "utf8");
+}
