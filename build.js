6591bcd42e64a97af7d80864f552941d9da833a1
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-26 19:32:11 +0200

    trying to register helper

--- build.js
+++ build.js
@@ -3,15 +3,22 @@ var handlebars = require("handlebars");
 var showdown = require("showdown");
 const converter = new showdown.Converter();
 
+handlebars.registerHelper('ifCond', function(v1, v2, options) {
+  if (v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 var data = fs.readFileSync("./content/data.json", "utf8");
 data = JSON.parse(data);
 
 // preprocess markdown
 var i = 0;
 data.entries.forEach(function(item) {
-    data.entries[i].body = converter.makeHtml(item.body);
-    console.log("ok");
-    i++;
+  data.entries[i].body = converter.makeHtml(item.body);
+  console.log("ok");
+  i++;
 });
 
 // docs
@@ -28,8 +35,8 @@ var style = fs.readFileSync("./src/assets/docs.css", "utf8");
 fs.writeFileSync("./public/assets/docs.css", style, "utf8");
 
 function compile(mydata, src, dest) {
-    var template = fs.readFileSync("./src/" + src, "utf8");
-    var pageBuilder = handlebars.compile(template);
-    var pageText = pageBuilder(mydata);
-    fs.writeFileSync("./public/" + dest, pageText, "utf8");
+  var template = fs.readFileSync("./src/" + src, "utf8");
+  var pageBuilder = handlebars.compile(template);
+  var pageText = pageBuilder(mydata);
+  fs.writeFileSync("./public/" + dest, pageText, "utf8");
 }
