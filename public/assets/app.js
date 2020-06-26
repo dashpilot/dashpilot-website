a02ee3a1f42a174a1f01b832455d875b8af3031c
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-27 00:26:54 +0200

    adding toggle

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,4 +1,14 @@
-document.querySelectorAll('pre code').forEach((block) => {
-  block.className = 'language-json';
-  hljs.highlightBlock(block);
+document.querySelectorAll('pre code').forEach((block) = > {
+  block.className = 'language-json';hljs.highlightBlock(block);
+});
+
+document.querySelectorAll('summary').forEach((item) => {
+  item.addEventListener('click', function() {
+    readMore = !readMore
+    if (readMore === true) {
+      item.innerHTML = 'Read less'
+    } else {
+      item.innerHTML = 'Read more'
+    }
+  });
 });
