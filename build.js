febf0dd4d469a976d6f2d1da351f3b1f065f916a
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2020-06-26 20:16:06 +0200

    Added the actual page title

--- build.js
+++ build.js
@@ -3,31 +3,26 @@ var handlebars = require("handlebars");
 var showdown = require("showdown");
 const converter = new showdown.Converter();
 
-handlebars.registerHelper('ifEq', function(v1, v2, options) {
-  if (v1 === v2) {
-    return options.fn(this);
-  }
-  return options.inverse(this);
-});
+handlebars.registerHelper('ifEq', function(v1, v2, options) { if (v1 === v2) { return options.fn(this); } return options.inverse(this); });
 
 var data = fs.readFileSync("./content/data.json", "utf8");
 data = JSON.parse(data);
 
 // preprocess markdown
 var i = 0;
-data.entries.forEach(function(item) {
-  data.entries[i].body = converter.makeHtml(item.body);
-  console.log("ok");
-  i++;
-});
+data.entries.forEach(function(item) { data.entries[i].body = converter.makeHtml(item.body);
+    console.log("ok");
+    i++; });
 
 // docs
 let mydata = {};
 mydata.entries = data.entries.filter((x) => x.category == "docs");
+mydata.page = 'Docs';
 compile(mydata, "template.html", "docs.html");
 
 // blog
 mydata.entries = data.entries.filter((x) => x.category == "blog");
+mydata.page = 'Blog';
 compile(mydata, "template.html", "blog.html");
 
 // copy style
@@ -35,8 +30,8 @@ var style = fs.readFileSync("./src/assets/docs.css", "utf8");
 fs.writeFileSync("./public/assets/docs.css", style, "utf8");
 
 function compile(mydata, src, dest) {
-  var template = fs.readFileSync("./src/" + src, "utf8");
-  var pageBuilder = handlebars.compile(template);
-  var pageText = pageBuilder(mydata);
-  fs.writeFileSync("./public/" + dest, pageText, "utf8");
+    var template = fs.readFileSync("./src/" + src, "utf8");
+    var pageBuilder = handlebars.compile(template);
+    var pageText = pageBuilder(mydata);
+    fs.writeFileSync("./public/" + dest, pageText, "utf8");
 }
