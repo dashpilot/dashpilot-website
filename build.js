bb29e2097a55ce7c463af79d52b2e093dfec3b30
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2022-03-07 17:34:44 +0100

    Replaced showdown with marked

--- build.js
+++ build.js
@@ -1,15 +1,14 @@
 var fs = require("fs");
 var handlebars = require("handlebars");
-var showdown = require("showdown");
-const converter = new showdown.Converter();
+const marked = require('marked');
 
 console.log("starting build...");
 
 handlebars.registerHelper("ifEq", function(v1, v2, options) {
-    if (v1 === v2) {
-        return options.fn(this);
-    }
-    return options.inverse(this);
+  if (v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
 });
 
 var data = fs.readFileSync("./public/content/data.json", "utf8");
@@ -18,8 +17,8 @@ data = JSON.parse(data);
 // preprocess markdown
 var i = 0;
 data.entries.forEach(function(item) {
-    data.entries[i].body = converter.makeHtml(item.body);
-    i++;
+  data.entries[i].body = marked.parse(item.body);
+  i++;
 });
 
 // docs
@@ -42,10 +41,10 @@ var js = fs.readFileSync("./src/assets/app.js", "utf8");
 fs.writeFileSync("./public/assets/app.js", js, "utf8");
 
 function compile(mydata, src, dest) {
-    var template = fs.readFileSync("./src/" + src, "utf8");
-    var pageBuilder = handlebars.compile(template);
-    var pageText = pageBuilder(mydata);
-    fs.writeFileSync("./public/" + dest, pageText, "utf8");
+  var template = fs.readFileSync("./src/" + src, "utf8");
+  var pageBuilder = handlebars.compile(template);
+  var pageText = pageBuilder(mydata);
+  fs.writeFileSync("./public/" + dest, pageText, "utf8");
 }
 
 console.log("build finished.");
